dd3d89ab2751dccc10435f19d3b573c0b35934ef
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-03-23 09:05:33 +0100

    add input capturing

--- Assets/Code/Inputs/InputHandler.cs
+++ Assets/Code/Inputs/InputHandler.cs
@@ -1,10 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviour {
 
-	
-	
-	
-	
+	public void OnMovementInput(InputAction.CallbackContext ctx) {
+
+		if (ctx.performed) {
+
+			// Prevent Diagonal Inputs
+			if (IsDiagonalInput(ctx.ReadValue<Vector2>()))
+				return;
+
+			Debug.Log(ctx.ReadValue<Vector2>());
+
+		}
+
+	}
+
+	public void OnRotationInput(InputAction.CallbackContext ctx) {
+
+		if (ctx.performed) {
+			Debug.Log(ctx.ReadValue<float>());
+		}
+
+	}
+
+	private bool IsDiagonalInput(Vector2 input) {
+		return input.x != 0 && input.y != 0;
+	}
 
 }
