2d23f3b142d85aa72e859fb2e28c0e21e49649ea
Ramon Brullo <kingvanti650@gmail.com>
2024-03-23 09:07:23 +0100

    add initial actor logic

--- Assets/Code/Actors/ActorRepo.cs
+++ Assets/Code/Actors/ActorRepo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -6,6 +7,36 @@ namespace DGJ24.Actors
 {
     internal class ActorRepo : MonoBehaviour, IActorRepo
     {
-        public IEnumerable<IActor> Actors { get; } = Enumerable.Empty<IActor>();
+        [SerializeField]
+        private GameObject[] initialActors = Array.Empty<GameObject>();
+
+        private readonly IList<IActor> actors = new List<IActor>();
+
+        public IEnumerable<IActor> Actors => actors;
+
+        private bool TryAddActor(GameObject actorGameObject)
+        {
+            var actor = GameObjectActor.TryMakeFrom(actorGameObject);
+            if (actor == null)
+                return false;
+
+            actors.Add(actor);
+            return true;
+        }
+
+        private void AddInitialActors()
+        {
+            foreach (var initialActor in initialActors)
+            {
+                var wasSuccess = TryAddActor(initialActor);
+                if (!wasSuccess)
+                    throw new Exception($"{initialActor.name} was not a valid actor gameobject.");
+            }
+        }
+
+        private void Awake()
+        {
+            AddInitialActors();
+        }
     }
 }
---
+++ Assets/Code/Actors/GameObjectActor.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace DGJ24.Actors
+{
+    internal class GameObjectActor : IActor
+    {
+        public static GameObjectActor? TryMakeFrom(GameObject actorGameObject)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
