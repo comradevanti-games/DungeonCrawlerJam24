56e055d23fffff203d0ad995193130ddb065c381
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-03-23 10:36:17 +0100

    add additional action request

--- Assets/Code/Actors/ActionRequests.cs
+++ Assets/Code/Actors/ActionRequests.cs
@@ -1,6 +1,10 @@
+using DGJ24.Inputs;
 using UnityEngine;
 
 namespace DGJ24.Actors
 {
     public abstract record ActionRequest(GameObject Actor);
+    public record MovementActionRequest(GameObject Actor, Direction Direction) : ActionRequest(Actor);
+    public record RotationActionRequest(GameObject Actor, Rotation Rotation) : ActionRequest(Actor);
+
 }
