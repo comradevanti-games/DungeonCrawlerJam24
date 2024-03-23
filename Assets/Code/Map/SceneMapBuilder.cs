fbe50589d7261a99436cb2ab02f8b376cc42c132
Ramon Brullo <kingvanti650@gmail.com>
2024-03-23 10:31:31 +0100

    add map building

--- Assets/Code/Map/SceneMapBuilder.cs
+++ Assets/Code/Map/SceneMapBuilder.cs
@@ -1,6 +1,42 @@
+using System;
 using UnityEngine;
 
 namespace DGJ24.Map
 {
-    internal class SceneMapBuilder : MonoBehaviour { }
+    internal class SceneMapBuilder : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject floorPrefab = null!;
+
+        [SerializeField]
+        private GameObject wallPrefab = null!;
+
+        private void BuildMap()
+        {
+            var blueprint = MapGen.Generate(new MapGen.Config());
+
+            foreach (var (tilePosition, tileType) in blueprint.Tiles)
+            {
+                var prefab = tileType switch
+                {
+                    TileType.Wall => wallPrefab,
+                    TileType.Floor => floorPrefab,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+
+                var position = new Vector3(
+                    tilePosition.x,
+                    prefab.transform.position.y,
+                    tilePosition.y
+                );
+
+                Instantiate(prefab, position, Quaternion.identity);
+            }
+        }
+
+        private void Start()
+        {
+            BuildMap();
+        }
+    }
 }
