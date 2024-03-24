303ddca7367df290e91d8d5a3fcc3f34a7b5ca18
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-03-24 11:12:29 +0100

    add test movement

--- Assets/Code/Actors/ActionDirector.cs
+++ Assets/Code/Actors/ActionDirector.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace DGJ24.Actors {
@@ -8,23 +10,71 @@ namespace DGJ24.Actors {
 			Singletons.Get<IActionMonitor>().ActionBatchReady += TryExecute;
 		}
 
-		public static void TryExecute(IActionMonitor.ActionBatchReadyEvent batch) {
+		private void TryExecute(IActionMonitor.ActionBatchReadyEvent batch) {
 			// Check for conflicts
 
 			foreach (ActionRequest? action in batch.Batch) {
 
 				switch (action) {
 
-					case MovementActionRequest move:
-
-						if (move.Direction == Direction.Forward) {
-							Debug.Log("Dequeued A Forward Move Event!");
-						}
-
+					case MovementActionRequest request:
+						MoveActor(request.Actor, request.Direction);
 						break;
+					case RotationActionRequest request:
+						RotateActor(request.Actor, request.Rotation);
+						break;
+
 				}
 
 			}
+
+		}
+
+		private void MoveActor(GameObject actor, Direction direction) {
+
+			Vector3 actorPosition = actor.transform.position;
+
+			switch (direction) {
+				
+				//TODO: Use Real Movement
+
+				case Direction.Forward:
+					StartCoroutine(LerpPosition(actor, actor.transform.position, new Vector3(actorPosition.x, actorPosition.y, actorPosition.z + 1),
+						0.2f));
+					break;
+				case Direction.Backward:
+					StartCoroutine(LerpPosition(actor, actor.transform.position, new Vector3(actorPosition.x, actorPosition.y, actorPosition.z - 1),
+						0.2f));
+					break;
+				case Direction.Left:
+					StartCoroutine(LerpPosition(actor, actor.transform.position, new Vector3(actorPosition.x - 1, actorPosition.y, actorPosition.z),
+						0.2f));
+					break;
+				case Direction.Right:
+					StartCoroutine(LerpPosition(actor, actor.transform.position, new Vector3(actorPosition.x + 1, actorPosition.y, actorPosition.z),
+						0.2f));
+					break;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+
+			}
+
+		}
+
+		private static void RotateActor(GameObject actor, Rotation rotation) { }
+
+		private static IEnumerator LerpPosition(GameObject actor, Vector3 origin, Vector3 targetPosition, float duration) {
+
+			float startTime = Time.time;
+
+			while (Time.time - startTime < duration) {
+				float progress = (Time.time - startTime) / duration;
+				actor.transform.position = Vector3.Lerp(origin, targetPosition, progress);
+				yield return null;
+			}
+
+			actor.transform.position = targetPosition;
+
 		}
 
 	}
