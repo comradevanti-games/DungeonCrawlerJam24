093be0b2067254c13fd823d62380a0ce72471a53
Ramon Brullo <kingvanti650@gmail.com>
2024-03-24 09:53:51 +0100

    add action-queue to actor

--- Assets/Code/Actors/GameObjectActor.cs
+++ Assets/Code/Actors/GameObjectActor.cs
@@ -4,9 +4,15 @@ namespace DGJ24.Actors
 {
     internal class GameObjectActor : IActor
     {
+        public IActionRequestQueue ActionRequestQueue { get; private set; } = null!;
+
         public static GameObjectActor? TryMakeFrom(GameObject actorGameObject)
         {
-            return new GameObjectActor();
+            var queue = actorGameObject.GetComponent<IActionRequestQueue>();
+            if (queue == null)
+                return null;
+
+            return new GameObjectActor { ActionRequestQueue = queue };
         }
     }
 }
--- Assets/Code/Actors/IActor.cs
+++ Assets/Code/Actors/IActor.cs
@@ -1,11 +1,13 @@
 namespace DGJ24.Actors
 {
-    
     /// <summary>
     /// An actor in the scene. Can execute actions
     /// </summary>
     public interface IActor
     {
-        
+        /// <summary>
+        /// The actors action-request queue.
+        /// </summary>
+        public IActionRequestQueue ActionRequestQueue { get; }
     }
 }
