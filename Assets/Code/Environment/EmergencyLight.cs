904fa36c885d2fad32ead36002a9c057d854dad4
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-03-25 16:08:33 +0100

    add flicker

--- Assets/Code/Environment/EmergencyLight.cs
+++ Assets/Code/Environment/EmergencyLight.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace DGJ24.Environment {
@@ -5,8 +6,50 @@ namespace DGJ24.Environment {
 	public class EmergencyLight : MonoBehaviour {
 
 		[SerializeField] private Light light;
+		[SerializeField] private float baseIntensity = 25f;
+		[SerializeField] private float minIntensity = 0.5f;
+		[SerializeField] private float maxIntensity = 1f;
 
-		private void Update() { }
+		public float minFlickerInterval = 0.5f;
+		public float maxFlickerInterval = 10f;
+		private float timeUntilNextFlicker;
+
+		private bool isRunning = false;
+
+		private void Start() {
+			timeUntilNextFlicker = Random.Range(minFlickerInterval, maxFlickerInterval);
+			StartCoroutine(Flicker());
+		}
+
+		IEnumerator Flicker() {
+
+			isRunning = true;
+
+			while (isRunning) {
+
+				timeUntilNextFlicker -= Time.deltaTime;
+
+				if (timeUntilNextFlicker <= 0) {
+
+					light.intensity = Random.Range(minIntensity, maxIntensity);
+					yield return new WaitForSeconds(0.2f);
+					light.intensity = Random.Range(minIntensity, maxIntensity);
+					yield return new WaitForSeconds(0.07f);
+					light.intensity = Random.Range(minIntensity, maxIntensity);
+					yield return new WaitForSeconds(0.145f);
+					light.intensity = Random.Range(minIntensity, maxIntensity);
+					yield return new WaitForSeconds(2f);
+
+					light.intensity = baseIntensity;
+					timeUntilNextFlicker = Random.Range(minFlickerInterval, maxFlickerInterval);
+
+				}
+
+				yield return null;
+
+			}
+
+		}
 
 	}
 
