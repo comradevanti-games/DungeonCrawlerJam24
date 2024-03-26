790f674500bab95c65e6afb50135574afed4005c
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-03-26 10:27:10 +0100

    add health display event

--- Assets/Code/UI/Game/HealthDisplay.cs
+++ Assets/Code/UI/Game/HealthDisplay.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthDisplay : MonoBehaviour {
+namespace DGJ24.UI {
 
-	// Start is called before the first frame update
-	void Start() { }
+	public class HealthDisplay : MonoBehaviour {
 
-	// Update is called once per frame
-	void Update() { }
+		public void OnPlayerHealthChanged(int newHealth) { }
+
+	}
 
 }
