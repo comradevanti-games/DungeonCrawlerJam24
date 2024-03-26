80933dc6b73436f51179dc858c9c1e114e0f2d1f
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-03-26 10:57:21 +0100

    add player health bar

--- Assets/Code/UI/Game/HealthDisplay.cs
+++ Assets/Code/UI/Game/HealthDisplay.cs
@@ -1,12 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace DGJ24.UI {
 
 	public class HealthDisplay : MonoBehaviour {
 
-		public void OnPlayerHealthChanged(int newHealth) { }
+		[SerializeField] private GameObject[]? healthPieces;
+
+		public void OnPlayerHealthChanged(int newHealth) {
+
+			for (int i = 0; i < healthPieces?.Length; i++) {
+				healthPieces[i].SetActive(i < newHealth);
+			}
+
+		}
 
 	}
 
