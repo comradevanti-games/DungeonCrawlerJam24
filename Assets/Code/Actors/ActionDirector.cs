cd9f3ab891275164966d6e683eac20272ca97475
Ramon Brullo <kingvanti650@gmail.com>
2024-03-26 11:07:26 +0100

    add no-op action

--- Assets/Code/Actors/ActionDirector.cs
+++ Assets/Code/Actors/ActionDirector.cs
@@ -5,170 +5,233 @@ using System.Linq;
 using DGJ24.Tools;
 using UnityEngine;
 
-namespace DGJ24.Actors {
+namespace DGJ24.Actors
+{
+    public class ActionDirector : MonoBehaviour
+    {
+        private HashSet<GameObject> ActivityPool { get; } = new();
+        private Queue<IActionMonitor.ActionBatchReadyEvent> ActionBatchQueue { get; } = new();
 
-	public class ActionDirector : MonoBehaviour {
+        private void Awake()
+        {
+            Singletons.Get<IActionMonitor>().ActionBatchReady += TryExecute;
+        }
 
-		private HashSet<GameObject> ActivityPool { get; } = new();
-		private Queue<IActionMonitor.ActionBatchReadyEvent> ActionBatchQueue { get; } = new();
+        private void Update()
+        {
+            if (!ActivityPool.Any() && ActionBatchQueue.Count == 0)
+                return;
 
-		private void Awake() {
-			Singletons.Get<IActionMonitor>().ActionBatchReady += TryExecute;
-		}
+            if (ActionBatchQueue.Count > 0)
+            {
+                TryExecute(ActionBatchQueue.Dequeue());
+            }
+        }
 
-		private void Update() {
+        private void TryExecute(IActionMonitor.ActionBatchReadyEvent batch)
+        {
+            if (ActivityPool.Count > 0)
+            {
+                ActionBatchQueue.Enqueue(batch);
+                return;
+            }
 
-			if (!ActivityPool.Any() && ActionBatchQueue.Count == 0) return;
+            foreach (ActionRequest? action in batch.Batch)
+            {
+                ActivityPool.Add(action.Actor);
 
-			if (ActionBatchQueue.Count > 0) {
-				TryExecute(ActionBatchQueue.Dequeue());
-			}
+                switch (action)
+                {
+                    case MovementActionRequest request:
+                        MoveActor(request.Actor, request.Direction, request.MoveDuration);
+                        break;
+                    case RotationActionRequest request:
+                        RotateActor(request.Actor, request.Rotation, request.RotateDuration);
+                        break;
+                    case TorchActionRequest request:
+                        UseTorch(request.Actor, OnActionRequestExecuted);
+                        break;
+                    case InteractionActionRequest request:
+                        Interact(request.Actor, request.TilePositions);
+                        break;
+                    case NoOpActionRequest:
+                        OnActionRequestExecuted(action.Actor);
+                        break;
+                    default:
+                        throw new ArgumentException(
+                            $"Unhandled action-request type: {action.GetType().Name}"
+                        );
+                }
+            }
+        }
 
-		}
+        private void MoveActor(GameObject actor, Direction direction, float duration)
+        {
+            Vector3 actorPosition = actor.transform.position;
 
-		private void TryExecute(IActionMonitor.ActionBatchReadyEvent batch) {
+            switch (direction)
+            {
+                case Direction.Forward:
+                    StartCoroutine(
+                        LerpPosition(
+                            actor,
+                            actorPosition,
+                            actorPosition + (actor.transform.forward * 2f),
+                            duration,
+                            OnActionRequestExecuted
+                        )
+                    );
+                    break;
+                case Direction.Backward:
+                    StartCoroutine(
+                        LerpPosition(
+                            actor,
+                            actorPosition,
+                            actorPosition + (actor.transform.forward * -2f),
+                            duration,
+                            OnActionRequestExecuted
+                        )
+                    );
+                    break;
+                case Direction.Left:
+                    StartCoroutine(
+                        LerpPosition(
+                            actor,
+                            actorPosition,
+                            actorPosition + (actor.transform.right * -2f),
+                            duration,
+                            OnActionRequestExecuted
+                        )
+                    );
+                    break;
+                case Direction.Right:
+                    StartCoroutine(
+                        LerpPosition(
+                            actor,
+                            actorPosition,
+                            actorPosition + (actor.transform.right * 2f),
+                            duration,
+                            OnActionRequestExecuted
+                        )
+                    );
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+            }
+        }
 
-			if (ActivityPool.Count > 0) {
-				ActionBatchQueue.Enqueue(batch);
-				return;
-			}
+        private void RotateActor(GameObject actor, Rotation rotation, float duration)
+        {
+            Quaternion origin = actor.transform.rotation;
 
-			foreach (ActionRequest? action in batch.Batch) {
+            switch (rotation)
+            {
+                case Rotation.Right:
+                {
+                    Quaternion targetRotation =
+                        actor.transform.rotation * Quaternion.Euler(0, 90, 0);
+                    StartCoroutine(
+                        LerpRotation(
+                            actor,
+                            origin,
+                            targetRotation,
+                            duration,
+                            OnActionRequestExecuted
+                        )
+                    );
+                    return;
+                }
+                case Rotation.Left:
+                {
+                    Quaternion targetRotation =
+                        actor.transform.rotation * Quaternion.Euler(0, -90, 0);
+                    StartCoroutine(
+                        LerpRotation(
+                            actor,
+                            origin,
+                            targetRotation,
+                            duration,
+                            OnActionRequestExecuted
+                        )
+                    );
+                    break;
+                }
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(rotation), rotation, null);
+            }
+        }
 
-				ActivityPool.Add(action.Actor);
+        private void UseTorch(GameObject actor, Action<GameObject> callback)
+        {
+            actor.GetComponentInChildren<Torch>().Flash();
+            callback.Invoke(actor);
+        }
 
-				switch (action) {
+        private void Interact(GameObject actor, Vector2Int[] interactedTiles)
+        {
+            GameObject? tileObject = FindObjectsByType<TileTransform>(
+                    FindObjectsInactive.Exclude,
+                    FindObjectsSortMode.None
+                )
+                .FirstOrDefault(tileTransform =>
+                    interactedTiles.Any(x => tileTransform.Position == x)
+                )
+                ?.gameObject;
 
-					case MovementActionRequest request:
-						MoveActor(request.Actor, request.Direction, request.MoveDuration);
-						break;
-					case RotationActionRequest request:
-						RotateActor(request.Actor, request.Rotation, request.RotateDuration);
-						break;
-					case TorchActionRequest request:
-						UseTorch(request.Actor, OnActionRequestExecuted);
-						break;
-					case InteractionActionRequest request:
-						Interact(request.Actor, request.TilePositions);
-						break;
-					default:
-						OnActionRequestExecuted(action.Actor);
-						break;
+            // TODO: Interact with other Objects based on what they are.
+            OnActionRequestExecuted(actor);
+        }
 
-				}
+        private void OnActionRequestExecuted(GameObject actor)
+        {
+            if (ActivityPool.Contains(actor))
+            {
+                ActivityPool.Remove(actor);
+            }
+        }
 
-			}
+        private static IEnumerator LerpPosition(
+            GameObject actor,
+            Vector3 origin,
+            Vector3 targetPosition,
+            float duration,
+            Action<GameObject> callback
+        )
+        {
+            float startTime = Time.time;
+            float endTime = startTime + duration;
 
-		}
+            while (Time.time < endTime)
+            {
+                float progress = (Time.time - startTime) / duration;
+                actor.transform.position = Vector3.Lerp(origin, targetPosition, progress);
+                yield return null;
+            }
 
-		private void MoveActor(GameObject actor, Direction direction, float duration) {
+            actor.transform.position = targetPosition;
+            callback.Invoke(actor);
+        }
 
-			Vector3 actorPosition = actor.transform.position;
+        private static IEnumerator LerpRotation(
+            GameObject actor,
+            Quaternion origin,
+            Quaternion targetRotation,
+            float duration,
+            Action<GameObject> callback
+        )
+        {
+            float startTime = Time.time;
+            float endTime = startTime + duration;
 
-			switch (direction) {
+            while (Time.time < endTime)
+            {
+                float progress = (Time.time - startTime) / duration;
+                actor.transform.rotation = Quaternion.Lerp(origin, targetRotation, progress);
+                yield return null;
+            }
 
-				case Direction.Forward:
-					StartCoroutine(LerpPosition(actor, actorPosition, actorPosition + (actor.transform.forward * 2f), duration,
-						OnActionRequestExecuted));
-					break;
-				case Direction.Backward:
-					StartCoroutine(LerpPosition(actor, actorPosition, actorPosition + (actor.transform.forward * -2f), duration,
-						OnActionRequestExecuted));
-					break;
-				case Direction.Left:
-					StartCoroutine(LerpPosition(actor, actorPosition, actorPosition + (actor.transform.right * -2f), duration,
-						OnActionRequestExecuted));
-					break;
-				case Direction.Right:
-					StartCoroutine(
-						LerpPosition(actor, actorPosition, actorPosition + (actor.transform.right * 2f), duration, OnActionRequestExecuted));
-					break;
-				default:
-					throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
-
-			}
-
-		}
-
-		private void RotateActor(GameObject actor, Rotation rotation, float duration) {
-
-			Quaternion origin = actor.transform.rotation;
-
-			switch (rotation) {
-
-				case Rotation.Right: {
-					Quaternion targetRotation = actor.transform.rotation * Quaternion.Euler(0, 90, 0);
-					StartCoroutine(LerpRotation(actor, origin, targetRotation, duration, OnActionRequestExecuted));
-					return;
-				}
-				case Rotation.Left: {
-					Quaternion targetRotation = actor.transform.rotation * Quaternion.Euler(0, -90, 0);
-					StartCoroutine(LerpRotation(actor, origin, targetRotation, duration, OnActionRequestExecuted));
-					break;
-				}
-				default:
-					throw new ArgumentOutOfRangeException(nameof(rotation), rotation, null);
-			}
-
-		}
-
-		private void UseTorch(GameObject actor, Action<GameObject> callback) {
-			actor.GetComponentInChildren<Torch>().Flash();
-			callback.Invoke(actor);
-		}
-
-		private void Interact(GameObject actor, Vector2Int[] interactedTiles) {
-
-			GameObject? tileObject = FindObjectsByType<TileTransform>(FindObjectsInactive.Exclude, FindObjectsSortMode.None)
-				.FirstOrDefault(tileTransform => interactedTiles.Any(x => tileTransform.Position == x))?.gameObject;
-
-			// TODO: Interact with other Objects based on what they are.
-			OnActionRequestExecuted(actor);
-
-		}
-
-		private void OnActionRequestExecuted(GameObject actor) {
-
-			if (ActivityPool.Contains(actor)) {
-				ActivityPool.Remove(actor);
-			}
-
-		}
-
-		private static IEnumerator LerpPosition(GameObject actor, Vector3 origin, Vector3 targetPosition, float duration,
-			Action<GameObject> callback) {
-
-			float startTime = Time.time;
-			float endTime = startTime + duration;
-
-			while (Time.time < endTime) {
-				float progress = (Time.time - startTime) / duration;
-				actor.transform.position = Vector3.Lerp(origin, targetPosition, progress);
-				yield return null;
-			}
-
-			actor.transform.position = targetPosition;
-			callback.Invoke(actor);
-
-		}
-
-		private static IEnumerator LerpRotation(GameObject actor, Quaternion origin, Quaternion targetRotation, float duration,
-			Action<GameObject> callback) {
-
-			float startTime = Time.time;
-			float endTime = startTime + duration;
-
-			while (Time.time < endTime) {
-				float progress = (Time.time - startTime) / duration;
-				actor.transform.rotation = Quaternion.Lerp(origin, targetRotation, progress);
-				yield return null;
-			}
-
-			actor.transform.rotation = targetRotation;
-			callback.Invoke(actor);
-
-		}
-
-	}
-
+            actor.transform.rotation = targetRotation;
+            callback.Invoke(actor);
+        }
+    }
 }
--- Assets/Code/Actors/ActionRequests.cs
+++ Assets/Code/Actors/ActionRequests.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
 
-namespace DGJ24.Actors {
+namespace DGJ24.Actors
+{
+    public abstract record ActionRequest(GameObject Actor);
 
-	public abstract record ActionRequest(GameObject Actor);
+    /// <summary>
+    /// An action which does nothing.
+    /// </summary>
+    public record NoOpActionRequest(GameObject Actor) : ActionRequest(Actor);
 
-	public record MovementActionRequest(GameObject Actor, Direction Direction, float MoveDuration) : ActionRequest(Actor);
+    public record MovementActionRequest(GameObject Actor, Direction Direction, float MoveDuration)
+        : ActionRequest(Actor);
 
-	public record RotationActionRequest(GameObject Actor, Rotation Rotation, float RotateDuration) : ActionRequest(Actor);
+    public record RotationActionRequest(GameObject Actor, Rotation Rotation, float RotateDuration)
+        : ActionRequest(Actor);
 
-	public record InteractionActionRequest(GameObject Actor, params Vector2Int[] TilePositions) : ActionRequest(Actor);
+    public record InteractionActionRequest(GameObject Actor, params Vector2Int[] TilePositions)
+        : ActionRequest(Actor);
 
-	public record TorchActionRequest(GameObject Actor) : ActionRequest(Actor);
-
+    public record TorchActionRequest(GameObject Actor) : ActionRequest(Actor);
 }
