86ec56f8076485affd156c0493a766a697856e83
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-03-26 11:23:30 +0100

    add action executed event

--- Assets/Code/Actors/IActionMonitor.cs
+++ Assets/Code/Actors/IActionMonitor.cs
@@ -1,19 +1,23 @@
 using System;
 using System.Collections.Immutable;
 
-namespace DGJ24.Actors
-{
-    /// <summary>
-    /// Monitors all actors and raises an event when all have chosen an
-    /// action-request to perform.
-    /// </summary>
-    public interface IActionMonitor
-    {
-        public record ActionBatchReadyEvent(IImmutableSet<ActionRequest> Batch);
+namespace DGJ24.Actors {
 
-        /// <summary>
-        /// Event for when all actors have chosen an action-request.
-        /// </summary>
-        public event Action<ActionBatchReadyEvent> ActionBatchReady;
-    }
+	/// <summary>
+	/// Monitors all actors and raises an event when all have chosen an
+	/// action-request to perform.
+	/// </summary>
+	public interface IActionMonitor {
+
+		public record ActionBatchReadyEvent(IImmutableSet<ActionRequest> Batch);
+
+		/// <summary>
+		/// Event for when all actors have chosen an action-request.
+		/// </summary>
+		public event Action<ActionBatchReadyEvent> ActionBatchReady;
+
+		public event Action AllActionsExecuted;
+
+	}
+
 }
