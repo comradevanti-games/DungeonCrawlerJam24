695031b06757e2b41ddd7550309e7687bd4abe88
Ramon Brullo <kingvanti650@gmail.com>
2024-03-28 16:55:04 +0100

    refactor interaction-layer enum
    Make flags enum

--- Assets/Code/Interactables/InteractionLayer.cs
+++ Assets/Code/Interactables/InteractionLayer.cs
@@ -1,15 +1,13 @@
 using System;
 
-namespace DGJ24.Interactables {
-
-	[Serializable]
-	public enum InteractionLayer {
-
-		None,
-		Player,
-		Enemy,
-		Loot
-
-	}
-
+namespace DGJ24.Interactables
+{
+    [Flags]
+    public enum InteractionLayer
+    {
+        None = 0,
+        Player = 1,
+        Enemy = 2,
+        Loot = 4
+    }
 }
