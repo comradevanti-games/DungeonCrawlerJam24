7f5e3fa094730dac9f1dec13267d4e458509c27e
Ramon Brullo <kingvanti650@gmail.com>
2024-03-29 15:35:04 +0100

    refactor ai logic
    Merge files

--- Assets/Code/AI/Brains/BlockPathBrain.cs
+++ Assets/Code/AI/Brains/BlockPathBrain.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Linq;
 using DGJ24.Actors;
 using DGJ24.Interactables;
 using DGJ24.Map;
@@ -13,45 +13,71 @@ namespace DGJ24.AI
         [SerializeField]
         private float stopDistance;
 
-        private IAIBrain internalBrain = null!;
+        private IInteractor interactor = null!;
+        private ITileTransform tileTransform = null!;
+        private ITileTransform playerTransform = null!;
+        private IFloorPlan floorPlan = null!;
+        private IPathNavigator navigator = null!;
+        private IWalkableProvider walkableProvider = null!;
 
         public override ActionRequest? DetermineNextAction(IAIBrain.ThinkContext ctx)
         {
-            return internalBrain.DetermineNextAction(ctx);
+            // Attack player
+
+            if (interactor.PotentialInteractables.Any())
+                return new InteractionActionRequest(ctx.Actor);
+
+            // Block path
+
+            var tile = tileTransform.Position;
+            var playerTile = playerTransform.Position;
+            var distance = Vector2Int.Distance(tile, playerTile);
+
+            var isCloseToPlayer = distance < stopDistance;
+            var isInCorridor = floorPlan.IsCorridor(tile);
+            var isBlockingPlayer = isCloseToPlayer && isInCorridor;
+            if (isBlockingPlayer)
+                return new NoOpActionRequest(ctx.Actor);
+
+            // Move towards player
+
+            navigator.Target = playerTile;
+            var path = navigator.Path;
+            if (path == null)
+                return new NoOpActionRequest(ctx.Actor);
+
+            var nextTile = path.Targets.First();
+            if (!walkableProvider.IsWalkable(nextTile))
+            {
+                navigator.UpdatePath();
+                return new NoOpActionRequest(ctx.Actor);
+            }
+
+            var diff = nextTile - tileTransform.Position;
+            var dirToNextTile = TileSpaceMath.TryDirectionForVector(diff);
+            if (dirToNextTile == null)
+            {
+                navigator.UpdatePath();
+                return new NoOpActionRequest(ctx.Actor);
+            }
+
+            var turnDir = TileSpaceMath.TryRotationTowards(
+                tileTransform.Forward,
+                dirToNextTile.Value
+            );
+            if (turnDir == null)
+                return new MovementActionRequest(ctx.Actor, dirToNextTile.Value, 0.5f);
+            return new RotationActionRequest(ctx.Actor, turnDir.Value, 0.5f);
         }
 
         private void Awake()
         {
-            var navigator = gameObject.RequireComponent<IPathNavigator>();
-            var tileTransform = gameObject.RequireComponent<ITileTransform>();
-            var interactor = gameObject.RequireComponent<IInteractor>();
-            var playerTransform = GameObject
-                .FindWithTag("Player")
-                .RequireComponent<ITileTransform>();
-            var walkableProvider = Singletons.Get<IWalkableProvider>();
-            var floorPlan = Singletons.Get<IFloorPlan>();
-
-            var playerTransformTarget = new TransformTargetProvider(playerTransform);
-            var huntPlayerBrain = new NavigateToTargetBrain(
-                navigator,
-                walkableProvider,
-                tileTransform
-            );
-            var attackBrain = new InteractBrain(interactor);
-            var navigationBrain = new DistanceSwitchBrain(
-                playerTransformTarget,
-                tileTransform,
-                (ctx, distance) =>
-                {
-                    if (distance < stopDistance && floorPlan.IsCorridor(tileTransform.Position))
-                        return new NoOpActionRequest(ctx.Actor);
-
-                    navigator.Target = playerTransform.Position;
-                    return huntPlayerBrain.DetermineNextAction(ctx);
-                },
-                (_) => throw new Exception("Should always have target")
-            );
-            internalBrain = new FallbackBrain(attackBrain, navigationBrain);
+            interactor = gameObject.RequireComponent<IInteractor>();
+            tileTransform = gameObject.RequireComponent<ITileTransform>();
+            navigator = gameObject.RequireComponent<IPathNavigator>();
+            playerTransform = GameObject.FindWithTag("Player").RequireComponent<ITileTransform>();
+            floorPlan = Singletons.Get<IFloorPlan>();
+            walkableProvider = Singletons.Get<IWalkableProvider>();
         }
     }
 }
