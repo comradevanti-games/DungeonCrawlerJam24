cba19beba1e7747f76a15ba1f0c9b93c4216fb16
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2024-03-30 18:13:05 +0100

    add controls to main menu

--- Assets/Code/Menu.UI/MenuOptionController.cs
+++ Assets/Code/Menu.UI/MenuOptionController.cs
@@ -1,10 +1,14 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace DGJ24.Menu.UI {
 
 	internal class MenuOptionController : MonoBehaviour {
-		
+
+		[SerializeField] private GameObject tutorialDisplay = null!;
+		[SerializeField] private GameObject controlsDisplay = null!;
+
 		public void Play() {
 			SceneManager.LoadScene("Main");
 		}
@@ -12,8 +16,43 @@ namespace DGJ24.Menu.UI {
 		public void Quit() {
 			Application.Quit();
 		}
-		
-		
+
+		public void ToggleTutorial() {
+
+			if (controlsDisplay.activeSelf) {
+				ToggleControls();
+			}
+
+			tutorialDisplay.SetActive(!tutorialDisplay.activeSelf);
+		}
+
+		public void ToggleControls() {
+
+			if (tutorialDisplay.activeSelf) {
+				ToggleTutorial();
+			}
+
+			controlsDisplay.SetActive(!controlsDisplay.activeSelf);
+
+			if (controlsDisplay.activeSelf) {
+				StartCoroutine(UnfoldKeyBindings());
+			}
+			else {
+				foreach (Transform t in controlsDisplay.GetComponentsInChildren<Transform>()) {
+					t.gameObject.SetActive(false);
+				}
+			}
+
+		}
+
+		private IEnumerator UnfoldKeyBindings() {
+			
+			foreach (RectTransform t in controlsDisplay.GetComponentsInChildren<RectTransform>(true)) {
+				t.gameObject.SetActive(true);
+				yield return new WaitForSeconds(0.005f);
+			}
+
+		}
 
 	}
 
